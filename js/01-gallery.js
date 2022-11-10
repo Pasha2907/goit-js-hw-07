18d651487ce29e552539cb4a2fbaf35227965886
Pasha2907 <pavliksischuk2001@gmail.com>
2022-11-10 18:56:14 +0200

    Готове дз 7

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,46 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+const itemMarkup = createGaleryItem(galleryItems);
+gallery.insertAdjacentHTML("beforeend", itemMarkup);
+
+function createGaleryItem(items) {
+  return items
+    .map(({ preview, description, original }) => {
+      return `
+      <div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img class = "gallery__image"
+            src="${preview}"            
+            data-source="${original}"
+            alt="${description}"/>
+        </a>
+      </div>`;
+    })
+    .join("");
+}
+
+gallery.addEventListener("click", onGalleryLinkClick);
+
+function onGalleryLinkClick(evt) {
+  evt.preventDefault();
+  if (evt.target.nodeName !== "IMG") {
+    return;
+  }
+
+  const instance = basicLightbox.create(`
+	<img 
+    src=${evt.target.dataset.source}            
+    width = "800"
+    height = "600"
+  >`);
+  instance.show();
+
+  document.addEventListener("keydown", closeModal);
+  function closeModal(evt) {
+    if (evt.code !== "Escape") {
+      return;
+    }
+    instance.close();
+  }
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,32 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+const itemMarkup = createGaleryItem(galleryItems);
+gallery.insertAdjacentHTML("beforeend", itemMarkup);
+
+function createGaleryItem(items) {
+  return items
+    .map(({ preview, description, original }) => {
+      return `
+        <a class="gallery__link" href="${original}">
+          <img class = "gallery__image"
+            src="${preview}"            
+            alt="${description}"/>
+        </a>`;
+    })
+    .join("");
+}
+
+gallery.addEventListener("click", onGalleryLinkClick);
+
+function onGalleryLinkClick(evt) {
+  evt.preventDefault();
+  if (evt.target.nodeName !== "IMG") {
+    return;
+  }
+}
+var lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionPosition: "bottom",
+  captionDelay: 250,
+});
